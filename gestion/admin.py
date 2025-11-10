e7aef7e3ebe1d9ab4b56823a7f41c452a7be4376
brunodrov <brunote10d@gmail.com>
2025-11-10 19:18:43 -0300

    proyecto casi finalizado

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Usuario, Vehiculo, Mantenimiento, ReporteEstado
+from .models import Usuario, Vehiculo, Mantenimiento, ReporteEstado, Turno, Chequeo
 
 admin.site.register(Usuario)
 admin.site.register(Vehiculo)
 admin.site.register(Mantenimiento)
 admin.site.register(ReporteEstado)
+admin.site.register(Turno)
+admin.site.register(Chequeo)
+
 
